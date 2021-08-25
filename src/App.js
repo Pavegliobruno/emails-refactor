3894f03eaef1c57418830351d0814ef29e694653
Bruno Paveglio <pavegliobruno@gmail.com>
2021-08-25 14:14:24 -0300

    First deployed

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, {useState} from 'react';
+import './App.css';
+import {Input, Popover, Button, Typography} from 'antd';
+import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
+
+const {TextArea} = Input;
+const {Paragraph} = Typography;
+
+function App() {
+	const [state, setState] = useState('');
+
+	const onChange = (e) => {
+		setState(e.target.value.replace(/(\r\n|\n|\r)/gm, ', '));
+	};
+
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<TextArea
+					rows={10}
+					placeholder='Pegá los emails aquí.'
+					onChange={onChange}
+				/>
+
+				<Popover
+					content={<Paragraph copyable>{state}</Paragraph>}
+					title='Emails acomodados:'
+					trigger='click'
+				>
+					<Button>Acomodar!</Button>
+				</Popover>
+			</header>
+		</div>
+	);
+}
+
+export default App;
